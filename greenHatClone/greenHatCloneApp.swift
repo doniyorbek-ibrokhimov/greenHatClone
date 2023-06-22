96b459f0aed2790cf7a24b8b9cdfff725df8459b
Doniyorbek <ac29526@akfauniversity.org>
2023-06-22 23:45:23 +0500

    Initial Commit

---
+++ greenHatClone/greenHatCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  greenHatCloneApp.swift
+//  greenHatClone
+//
+//  Created by Doniyorbek Ibrokhimov  on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct greenHatCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
